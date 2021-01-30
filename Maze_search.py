2294dbc1fc4b6a13ba955f6d44340896741a6d2a
vijay <sahanivijay800@gmail.com>
2021-01-30 14:30:07 +0530

    Help:Infinite Recursion in dfs

--- Maze_search.py
+++ Maze_search.py
@@ -1,13 +1,13 @@
 
 
-class BFS:
+class Maze:
     def createMaze(self):
         maze = []
         maze.append(["#", "#", "#", "#", "#", "O", "#"])
         maze.append(["#", " ", " ", " ", "#", " ", "#"])
         maze.append(["#", " ", "#", " ", "#", " ", "#"])
         maze.append(["#", " ", "#", " ", " ", " ", "#"])
-        maze.append(["#", " ", "#", "#", "#", " ", "#"])
+        maze.append(["#", " ", "#", "#", "#", "#", "#"])
         maze.append(["#", " ", " ", " ", "#", "#", "#"])
         maze.append(["#", "X", "#", "#", "#", "#", "#"])
 
@@ -27,87 +27,74 @@ class BFS:
         maze.append(["#", "#", "#", "#", "#", "#", "#", "X", "#"])
         return maze
 
-
     def printMaze(self,maze, path=""):
         for index, pos in enumerate(maze[0]):
             if pos == "O":
                 start = index
+                break
         i = start
         j = 0
         pos = set()
         for move in path:
-            if move == "L":
-                i -= 1
-            elif move == "R":
-                i += 1
-            elif move == "U":
-                j -= 1
-            elif move == "D":
-                j += 1
+            j,i=self.path(move,i,j)
             pos.add((j, i))
         for i, row in enumerate(maze):
             for j, col in enumerate(row):
-                if maze[i][j]=="#" and i==0:
-                    col="_"
-                if maze[i][j]=="#" and i==len(maze)-1:
-                    col="‾"
                 if (i, j) in pos:
-                    print("+", end=" ")
+                    print("+", end="  ")
                 else:
-                    print(col+"", end=" ")
+                    print(col+"", end="  ")
             print()
 
+    def path(self,move,i,j):
+        if move == "L":
+            i -= 1
+        elif move == "R":
+            i += 1
+        elif move == "U":
+            j -= 1
+        elif move == "D":
+            j += 1
+        return (j,i)
+    
+    def valid(self,maze, moves):
+        for index, pos in enumerate(maze[0]):
+            if pos == "O":
+                start = index
+                break
+        i = start
+        j = 0
+        # pos=set()  # To keep track of previous traversed pos
+        for move in moves:
+            j,i=self.path(move,i,j)
+            if not (0 <= i < len(maze[0]) and 0 <= j < len(maze)): #and (j,i) not in pos:
+                return False
+            elif maze[j][i] == "#":
+                return False
+            # pos.add((j,i))
+        return True
 
     def findend(self,maze, moves):
         for index, pos in enumerate(maze[0]):
             if pos == "O":
                 start = index
+                break
         i = start
         j = 0
         for move in moves:
-            if move == "L":
-                i -= 1
-            elif move == "R":
-                i += 1
-            elif move == "U":
-                j -= 1
-            elif move == "D":
-                j += 1
+            j,i=self.path(move,i,j)
         if maze[j][i] == "X":
             print("Found:", moves)
             self.printMaze(maze, moves)
+            exit()
             return True
         return False
 
-
-    def valid(self,maze, moves):
-        for index, pos in enumerate(maze[0]):
-            if pos == "O":
-                start = index
-        i = start
-        j = 0
-        for move in moves:
-            if move == "L":
-                i -= 1
-            elif move == "R":
-                i += 1
-            elif move == "U":
-                j -= 1
-            elif move == "D":
-                j += 1
-            if not (0 <= i < len(maze[0]) and 0 <= j < len(maze)):
-                return False
-            elif maze[j][i] == "#":
-                return False
-        return True
-
-
-# ** Main Algorithm
-def main():
-    bfs=BFS()
+#**  Main Algorithms
+def BFS(bfs):
     queue = [""]
     moves = ""
-    maze = bfs.createMaze()
+    maze = bfs.createMaze2()
     while not bfs.findend(maze, moves):
         moves = queue.pop(0)
         for i in ["L", "R", "U", "D"]:
@@ -115,8 +102,26 @@ def main():
             if bfs.valid(maze, move):
                 queue.append(move)
 
+# ? Help me fix DFS Recursive call
+def _dfs(traversal,maze,moves):
+    if not traversal.findend(maze,moves):
+        for i in ["U", "D", "L", "R"]:
+            move=moves+i
+            if traversal.valid(maze,move) :  
+                _dfs(traversal,maze,move) #! Infinity recursion
+
+
+def dfs(traversal):
+    visited=set()
+    maze=traversal.createMaze2()
+    return _dfs(traversal,maze,"")
+
 if __name__=="__main__":
-    main()
+    traversal=Maze()
+    BFS(traversal)
+    # dfs(traversal)
+
+
 
 
 # ‾
